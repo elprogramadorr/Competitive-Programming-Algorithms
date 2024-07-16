ee96787981c0213bc66a3b925b3e391d3d0512f5
elprogramadorr <91036558+elprogramadorr@users.noreply.github.com>
2024-07-16 17:15:18 -0400

    Update sort c-clockwise.cpp

--- Geometry/sort c-clockwise.cpp
+++ Geometry/sort c-clockwise.cpp
@@ -1,26 +1,18 @@
 bool up(Point a){
     return a.y > 0 || (a.y == 0 && a.x >= 0);
 }
-int cross(const Point &A, const Point &B) { return A.x*B.y - A.y*B.x; }
 bool cmp(Point a, Point b){
     if (up(a) != up(b)) return up(a) > up(b);
         return cross(a, b) > 0;
 }
 
-// no tan bueno
-struct Point{
-	int x,y;
-    long double A; 
-};
-long double PI = acos(-1);
-long double angle(Point x) {
-	long double a = atan2(x.y, x.x);
-	if (a < 0) {
-		a += 2*PI;
-	}
-	return a;
+// this starts from the half line 𝑥≤0, 𝑦=0 
+int group(Point a){
+    if(a.y<0)return -1;
+    if(a.y==0 && a.x>=0)return 0;
+    return 1;
 }
-int cross(const Point &A, const Point &B) { return A.x*B.y - A.y*B.x; }
-bool cmp(Point x, Point y){
-	return x.A < y.A;	
+bool cmp(Point a, Point b){
+    if(group(a)==group(b))return cross(a,b)>0;
+    return group(a)<group(b);
 }
